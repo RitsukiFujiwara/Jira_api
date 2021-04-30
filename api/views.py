0cd36085c4cc8be52987b730f810485548f33b9d
Fujiwara <r9.20important@icloud.com>
2021-04-30 11:23:19 +0900

    view修正

--- api/views.py
+++ api/views.py
@@ -1,3 +1,65 @@
 from django.shortcuts import render
+from rest_framework import status, permissions, generics, viewsets
+from .serializers import UserSerializer, CategorySerializer, TaskSerializer, ProfileSerializer
+from rest_framework.response import Response
+from .models import Task, Category, Profile
+from django.contrib.auth.models import User
+from . import costompermissions
 
-# Create your views here.
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (permissions.AllowAny,)
+
+class ListUserView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class LoginUserView(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user_profile=self.request.user)
+
+    def destroy(self, request, *args, **kwargs):
+        response = {'message': 'DELETE method is not allowed'}
+        return Response(response, state=status.HTTP_400_BAD_REQUEST)
+
+    def partial_update(self, request, *args, **kwargs):
+        response = {'message': 'DELETE method is not allowed'}
+        return Response(response, state=status.HTTP_400_BAD_REQUEST)
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.object.all()
+    serializer_class = CategorySerializer
+
+    def destroy(self, request, *args, **kwargs):
+        response = {'message': 'DELETE method is not allowed'}
+        return Response(response, state=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, *args, **kwargs):
+        response = {'message': 'DELETE method is not allowed'}
+        return Response(response, state=status.HTTP_400_BAD_REQUEST)
+
+    def partial_update(self, request, *args, **kwargs):
+        response = {'message': 'DELETE method is not allowed'}
+        return Response(response, state=status.HTTP_400_BAD_REQUEST)
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.object.all()
+    serializer_class = TaskSerializer
+    permission_classes = (permissions.IsAuthenticated, costompermissions.OwnerPermission,)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+    def partial_update(self, request, *args, **kwargs):
+        response = {'message': 'DELETE method is not allowed'}
+        return Response(response, state=status.HTTP_400_BAD_REQUEST)
+
